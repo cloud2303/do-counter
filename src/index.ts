aa33f581d767ff39acbfc06c36ae242bebeafe29
jiahui <2303058491@qq.com>
2024-04-06 21:10:44 +0800

    init counter

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@
  * Learn more at https://developers.cloudflare.com/durable-objects
  */
 
+import parseUrl from 'parse-url';
+import { DurableObject } from 'cloudflare:workers';
+
 /**
  * Associate bindings declared in wrangler.toml with the TypeScript type system
  */
@@ -16,7 +19,7 @@ export interface Env {
 	// MY_KV_NAMESPACE: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	MY_DURABLE_OBJECT: DurableObjectNamespace;
+	COUNTERS: DurableObjectNamespace<Counter>;
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
@@ -29,16 +32,22 @@ export interface Env {
 }
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier
-	 *
-	 * @param state - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
-	constructor(state: DurableObjectState, env: Env) {}
-
+export class Counter  extends DurableObject{
+	async getCounterValue(){
+		return (await this.ctx.storage.get<number>("value")) || 0
+	}
+	async increment(amount=1){
+		let value = (await this.ctx.storage.get<number>("value")) || 0;
+		value += amount;
+		await this.ctx.storage.put<number>("value", value);
+		return value;
+	}
+	async decrement(amount=1){
+		let value = (await this.ctx.storage.get<number>("value")) || 0;
+		value -= amount;
+		await this.ctx.storage.put<number>("value", value);
+		return value;
+	}
 	/**
 	 * The Durable Object fetch handler will be invoked when a Durable Object instance receives a
 	 * 	request from a Worker via an associated stub
@@ -47,6 +56,8 @@ export class MyDurableObject {
 	 * @returns The response to be sent back to the Worker
 	 */
 	async fetch(request: Request): Promise<Response> {
+		let params = parseUrl(request.url)
+		console.log(params)
 		return new Response('Hello World');
 	}
 }
@@ -63,16 +74,38 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		// We will create a `DurableObjectId` using the pathname from the Worker request
 		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+		let url = new URL(request.url);
+		console.log(url.searchParams)
+		let name = url.searchParams.get('name');
+		if(!name){
+				return new Response('Please enter a valid name');
+		}
+
+		let id = env.COUNTERS.idFromName(name);
 
 		// This stub creates a communication channel with the Durable Object instance
 		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub: DurableObjectStub = env.MY_DURABLE_OBJECT.get(id);
+		let stub = env.COUNTERS.get(id);
 
 		// We call `fetch()` on the stub to send a request to the Durable Object instance
 		// The Durable Object instance will invoke its fetch handler to handle the request
-		let response = await stub.fetch(request);
+		let count = null;
+		console.log(url.pathname)
+		switch(url.pathname){
+			case "/increment":
+				count = await stub.increment();
+				break;
+			case "/decrement":
+				count = await stub.decrement();
+				break;
+			case "/":
+				count = await stub.getCounterValue();
+				break;
 
-		return response;
+			default:
+				return new Response('Not Found',{status:404});
+
+		}
+		return new Response(`Durable Object '${name}' count: ${count}`);
 	},
 };
